a30f0c8dedbf8af905cb4a66e315e12c880a3d47
Dmitry O. Markov <markov.dm@inbox.ru>
2020-10-18 20:06:49 +0300

    add event delete button to current_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,4 @@ class User < ApplicationRecord
   has_many :events
 
   validates :name, presence: true, length: { maximum: 35 }
-
-  before_validation :set_name, on: :create
-
-  private
-
-  def set_name
-    self.name = "Товарисч #{rand(777)}" if self.name.blank?
-  end
 end
