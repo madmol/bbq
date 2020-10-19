7b97e4cfebbf677d19f56958418c0f5cb94e6dd1
Dmitry O. Markov <markov.dm@inbox.ru>
2020-10-19 19:49:29 +0300

    Intermediate subscription form

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :subscriptions
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :address, :datetime, :user, presence: true
