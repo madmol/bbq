82dd53599dd5e34612423e4991bdb75ae0a6dbe8
Dmitry O. Markov <markov.dm@inbox.ru>
2020-10-20 17:01:15 +0300

    subscription added and work

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,18 +3,26 @@ class SubscriptionsController < ApplicationController
   before_action :set_subscription, only: %i[destroy]
 
   def create
-    @subscription = Subscription.new(subscription_params)
-    
-    if @subscription.save
-      redirect_to @subscription, notice: I18n.t('controllers.subscriptions.created')
+    @new_subscription = @event.comments.build(subscription_params)
+    @new_subscription.user = current_user
+
+    if @new_subscription.save
+      redirect_to @event, notice: I18n.t('controllers.subscriptions.created')
     else
-      render :new
+      render 'events/show', alert: I18n.t('controllers.subscriptions.error')
     end
   end
 
   def destroy
-    @subscription.destroy
-    redirect_to subscription_url, notice: I18n.t('controllers.subscriptions.deleted')
+    message = { notice: I18n.t('controllers.subscriptions.destroyed') }
+
+    if current_user_can_edit?(@subscription)
+      @subscription.destroy!
+    else
+      message = { alert: I18n.t('controllers.subscriptions.error') }
+    end
+
+    redirect_to @event, message
   end
 
   private
