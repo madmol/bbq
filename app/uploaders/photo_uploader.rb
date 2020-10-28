3537ff0f89115e2ba25a8659862f58a760e2fbdd
Dmitry O. Markov <markov.dm@inbox.ru>
2020-10-28 17:02:37 +0300

    foq-aws for heroku

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,7 +4,12 @@ class PhotoUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
