a34d2a476116d6ebfa7c3c8f311c57109fdf71b5
Dmitry O. Markov <markov.dm@inbox.ru>
2020-10-31 01:46:28 +0300

    fixed domain in config actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,8 +80,8 @@ Rails.application.configure do
       :address        => ENV['MAILGUN_SMTP_SERVER'],
       :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
       :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-      :domain         => 'yourapp.heroku.com',
-      :authentication => :plain,
+      :domain         => 'bbqforeveryone.heroku.com',
+      :authentication => :plain
   }
 
   # Inserts middleware to perform automatic connection switching.
