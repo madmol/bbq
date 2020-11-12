e4f8006e3a25a1b7b5a69f22347d79fbfd338bf1
Dmitry O. Markov <markov.dm@inbox.ru>
2020-11-12 14:39:02 +0300

    added gallery_thumb2image.js

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -2,12 +2,10 @@ require("@rails/ujs").start()
 import 'bootstrap/dist/js/bootstrap'
 import '@fortawesome/fontawesome-free/js/all'
 import 'ekko-lightbox/dist/ekko-lightbox'
+import '../scripts/gallery_thumb2image'
 
 import '../styles/application'
 
 const images = require.context('../images', true)
 
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-  event.preventDefault();
-  $(this).ekkoLightbox();
-});
+
