6d92159aa9f332dd8a7601329ea3e32ffb10ab62
Dmitry O. Markov <markov.dm@inbox.ru>
2020-11-12 03:25:33 +0300

    moved lightbox js to application.js

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -6,3 +6,8 @@ import 'ekko-lightbox/dist/ekko-lightbox'
 import '../styles/application'
 
 const images = require.context('../images', true)
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
